ba03d33854dc4c7c37df5b97a7a3df37b02700c3
Samuel Vergara <samuelvergarar@gmail.com>
2024-10-24 16:53:11 -0500

    Se creo la seccion del admin para la app payment

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+admin.site.register(ShippingAddress)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
+class OrderItemInLine(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+# Ampliar el model de Order
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ["date_order"]
+    fields = ["user", "full_name", "email", "shipping_address", "amount_paid", "date_order", "shipped", "date_shipped"]
+    inlines = [OrderItemInLine]
+
+# Cancelar el Registro del model de aoarder
+admin.site.unregister(Order)
+
+# Volver a registrar Order y OrderItems
+admin.site.register(Order, OrderAdmin)
