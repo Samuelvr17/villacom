5f43cdff3e8dfe94c5e7aac5e840db7fa9e6be81
IFMB1 <ifmb982@hotmail.com>
2024-11-12 18:28:37 -0500

    base de datos desplegada en railway

--- ecom/settings.py
+++ ecom/settings.py
@@ -80,8 +80,15 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'LuDuIyJSKbOagnEPfmbOqVTKqCeaPkWc',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT':'31682',
+        
     }
 }
 
